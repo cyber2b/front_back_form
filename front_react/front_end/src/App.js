c5340d339b2dddb0a0ee044e563d2cb639f76744
balaji <balajirandy2002@gmail.com>
2024-11-04 12:45:25 +0530

    user in backend is created

--- front_react/front_end/src/App.js
+++ front_react/front_end/src/App.js
@@ -7,18 +7,18 @@ function App() {
       <header><h1>Form</h1></header>
       <form>
         <div className="content">
-        <label>Name:</label>
-        <input type="text" required></input>
+        <label for="name">Name:</label>
+        <input type="text" id="name" required></input>
         <br></br>
         </div>
         <div className="content">
-        <label>Email:</label>
-        <input type="email" required></input>
+        <label for ="mail">Email:</label>
+        <input type="email" id="mail" required></input>
        
          </div>
         <div className="content">
-        <label>Age:</label>
-        <input type="number" required></input>
+        <label for ="age">Age:</label>
+        <input type="number" id="age" required></input>
         </div>
         <div className="content" id="sub">
         <button type="submit">Submit</button>
