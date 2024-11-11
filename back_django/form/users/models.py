19cfb72a9920615fdaf7c5a6b5cb7f86526a02e8
balaji <balajirandy2002@gmail.com>
2024-11-11 14:01:56 +0530

    temp database is created

--- back_django/form/users/models.py
+++ back_django/form/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class login(models.Model):
+
+    name =models.CharField(max_length=10)
+    email=models.CharField(max_length=20)
+    password=models.CharField(max_length=8)
+
+
--- back_django/form/users/views.py
+++ back_django/form/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,HttpResponse
+from .models import login
 
 # Create your views here.
 
 def hello(request):
-
-    return HttpResponse("hello")
+    logins =login.objects.all()
+    return render(request,"users.html",{"posts":logins})
